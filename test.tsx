ac43e9f81bc02427406abb8e8b64474543e33cad
supercoda <andreljx1986@gmail.com>
2025-01-15 16:28:14 +1100

    Update test.tsx

--- test.tsx
+++ test.tsx
@@ -1,5 +1,21 @@
-export const password = 'testpass';
-
-const server = http.createServer((req, res) => {
-  send(req, req.url, options);
+var express = require('express');
+var router = express.Router();
+ 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+ // Get parameter. In this case, the image name.
+ const image = req.query.image;
+ // Execute the command
+ exec(`git log --oneline ${image}`, (err, output) => {
+   // Respond with HTTP 500 if there was an error
+   if (err) {
+     res.status(500).send(err);
+ 
+     return;
+   }
+   // output the HTTP response
+   res.send(output);
+ });
 });
+ 
+module.exports = router;
